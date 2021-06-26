f64ebca1ab6ed12b03452e19623adf29554e67c5
Terry Theis <tltheis@prodigy.net>
2021-06-26 09:47:50 -0500

    add Unified Topology from node warning

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
+  //useFindAndModify: false
 });
 
 // routes
